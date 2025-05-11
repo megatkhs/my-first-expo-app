27d448af5ebc4a69f90aa8fe5259d686d14b9a8d
megatkhs <tarodino0526@gmail.com>
2025-05-11 12:28:40 +0900

    ナビゲーション周りの動作を確認

---
+++ app/(tabs)/(home)/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return <View style={styles.container}>
+        <Text>ホーム画面</Text>
+      <Link
+        href={{
+          pathname: '/details/[id]',
+          params: { id: 'bacon' },
+        }}>
+        View user details
+      </Link>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red",
+    }
+})
---
+++ app/(tabs)/settings/index.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function SettingsScreen() {
+    return <View><Text>設定画面</Text></View>
+}
