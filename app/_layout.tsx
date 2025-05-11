869e48da4615511757a9e3d3ef02cecc60ae40e8
megatkhs <tarodino0526@gmail.com>
2025-05-11 14:41:37 +0900

    expo-navigation-barを追加し、Androidプラットフォームでのナビゲーションバーのスタイルを設定するuseEffectフックを実装

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
+import * as NavigationBar from 'expo-navigation-bar';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import type React from 'react';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setStyle('inverted');
+    }
+  }, []);
+
   return (
     <>
       <Stack
@@ -8,7 +19,9 @@ export default function RootLayout() {
           headerShown: false,
         }}
       >
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
   );
 }
