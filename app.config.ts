46dfd3173db57485c10e46c747d36cb50da86d09
megatkhs <ryo.takahashi@megatkhs.com>
2025-05-11 21:25:18 +0900

    slugを変更

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,20 @@
 // 静的な設定はapp.json、環境によって変更する場合はこのファイルで指定する
 import type { ConfigContext, ExpoConfig } from 'expo/config';
 
-const APP_PREFIX_MAP = {
+const APP_NAME_PREFIX_MAP = {
   development: '(DEV)',
   test: '(STG)',
   production: '',
 };
 
+const APP_SLUG_PREFIX_MAP = {
+  development: '-dev',
+  test: '-stg',
+  production: '',
+};
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  slug: `${config.slug}${APP_PREFIX_MAP[process.env.NODE_ENV]}`,
-  name: `${config.name}${APP_PREFIX_MAP[process.env.NODE_ENV]}`,
+  name: `${config.name}${APP_NAME_PREFIX_MAP[process.env.NODE_ENV]}`,
+  slug: `${config.slug}${APP_SLUG_PREFIX_MAP[process.env.NODE_ENV]}`,
 });
