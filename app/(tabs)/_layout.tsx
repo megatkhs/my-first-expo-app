dce137d860c2c7199b79fe88462c5c75edaab754
megatkhs <tarodino0526@gmail.com>
2025-05-11 14:40:37 +0900

    タブバーにハプティックフィードバックを追加し、色のスキームに基づいてスタイルを調整

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,47 @@
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { Tabs } from 'expo-router';
 import type React from 'react';
+import { Platform } from 'react-native';
 
 export default function TabsLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarBackground: TabBarBackground,
+        tabBarButton: HapticTab,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: 'absolute',
+          },
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="(home)"
+        options={{
+          title: 'ホーム',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="house.fill" color={color} />
+          ),
         }}
-      >
-        <Tabs.Screen name="(home)" />
-        <Tabs.Screen name="settings" />
-      </Tabs>
-    </>
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: '設定',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={24} name="gear" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
